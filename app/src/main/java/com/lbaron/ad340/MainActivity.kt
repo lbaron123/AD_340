4ee8a35b955c9436118f3fa10d30d2ca42dacef0
lbaron123 <liam_baron@hotmail.co.uk>
2020-11-05 17:57:46 +0000

    Changed README.md
    Signed-off-by: lbaron123 <liam_baron@hotmail.co.uk>

--- app/src/main/java/com/lbaron/ad340/MainActivity.kt
+++ app/src/main/java/com/lbaron/ad340/MainActivity.kt
@@ -3,10 +3,16 @@ package com.lbaron.ad340
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+// : = extends meaning inherits from AppCompatActivity
 class MainActivity : AppCompatActivity() {
+    //eg below we are overriding the onCreate method
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // this method makes the view
+        // this method makes the view. We are passing a LAYOUT file into contentView
+        //R = resources
+        //layout - layout resource
+        //could be eg string = string resourse or color resource, then the NAME of it in purple
         setContentView(R.layout.activity_main)
     }
 }

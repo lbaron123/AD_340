8ac0b7e384c6318701f9af4cef7cf3994170c306
lbaron123 <liam_baron@hotmail.co.uk>
2020-11-06 14:32:54 +0000

    UPdate
    Signed-off-by: lbaron123 <liam_baron@hotmail.co.uk>

--- app/src/main/java/com/lbaron/ad340/MainActivity.kt
+++ app/src/main/java/com/lbaron/ad340/MainActivity.kt
@@ -2,10 +2,17 @@ package com.lbaron.ad340
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 
 // : = extends meaning inherits from AppCompatActivity
 class MainActivity : AppCompatActivity() {
+
+    // region Setup Methods
+
     //eg below we are overriding the onCreate method
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +21,43 @@ class MainActivity : AppCompatActivity() {
         //layout - layout resource
         //could be eg string = string resourse or color resource, then the NAME of it in purple
         setContentView(R.layout.activity_main)
+
+        val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
+
+        enterButton.setOnClickListener {
+            //this - refers to this activity(main)
+            //stores the text of user input to a string called zipcode
+            val zipcode: String = zipcodeEditText.text.toString()
+
+            if(zipcode.length != 5){
+                Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
+            } else{
+                // Toast gives quick feedback to users
+                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+    // endregion Setup Methods
+    // region Teardown Methods
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+    // endregion Teardown Methods
 }
